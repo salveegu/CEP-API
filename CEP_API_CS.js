7aeea0cca9b46c73de8981b5a01dd5903d7578ca
pedro bzz <pedrobzz66@gmail.com>
2022-06-06 16:03:08 -0300

    16:02 segunda-feira, 6 de junho de 2022 (BRT) Horário em Balneário Camboriú, SC

---
+++ CEP_API_CS.js
@@ -0,0 +1,40 @@
+const fieldCep = ZDK.Page.getField("CEP_API").getValue();
+let formattedCep = fieldCep.replace(/\D+/g, "");
+const sizeFormattedCep = formattedCep.toString().length;
+
+ZDK.Client.showAlert(formattedCep);
+console.log("Tamanho do Campo: "+sizeFormattedCep);
+
+if (sizeFormattedCep == 'null' || sizeFormattedCep == 0 || sizeFormattedCep == 'Null') {
+   return ZDK.Client.showAlert(sizeFormattedCep+ "Campo vazio!");
+}
+
+if (sizeFormattedCep>=8 && sizeFormattedCep<=10) {
+    ZDK.Client.showAlert("Quantidade de digitos válidos!");
+    ZDK.Client.showAlert(formattedCep);
+
+    let verificationCepApi = ZDK.HTTP.request({
+        url: `https://viacep.com.br/ws/${formattedCep}/json`,
+        method: 'GET'
+        
+    }).getResponse();
+
+    ZDK.Client.showAlert(verificationCepApi);
+
+    jsonCep = JSON.parse(verificationCepApi);
+
+    ZDK.Page.getField("Cep_APi_CEP").setValue(jsonCep.cep);
+    ZDK.Page.getField("Logradouro_API_CEP").setValue(jsonCep.logradouro);
+    ZDK.Page.getField("Complemento_API_CEP").setValue(jsonCep.complemento);
+    ZDK.Page.getField("Bairro_API_CEP").setValue(jsonCep.bairro);
+    ZDK.Page.getField("Uf_API_CEP").setValue(jsonCep.uf);
+    ZDK.Page.getField("Localidade_API_CEP").setValue(jsonCep.localidade);
+    ZDK.Page.getField("Ibge_API_CEP").setValue(jsonCep.ibge);
+    ZDK.Page.getField("Gia_API_CEP").setValue(jsonCep.gia);
+    ZDK.Page.getField("Ddd_API_CEP").setValue(jsonCep.ddd);
+    ZDK.Page.getField("Siafi_API_CEP").setValue(jsonCep.siafi);
+   
+
+} else {
+   return ZDK.Client.showAlert("CEP Inválido!");
+}
